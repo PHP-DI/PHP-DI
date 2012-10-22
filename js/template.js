989f49dfcaf5356d51740b7e1bae43d07082c36e
Matthieu Napoli <matthieu@mnapoli.fr>
2012-10-22 17:04:18 +0200

    Pages generation

---
+++ js/template.js
@@ -0,0 +1,22 @@
+$(document).scroll(function(){
+    // If has not activated (has no attribute "data-top"
+    if (!$('.subnav').attr('data-top')) {
+        // If already fixed, then do nothing
+        if ($('.subnav').hasClass('subnav-fixed')) return;
+        // Remember top position
+        var offset = $('.subnav').offset()
+        $('.subnav').attr('data-top', offset.top);
+    }
+
+    if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
+        $('.subnav').addClass('subnav-fixed');
+    else
+        $('.subnav').removeClass('subnav-fixed');
+});
+
+$(function() {
+	$("section h1").wrap('<div class="page-header" />');
+	$("section p:first-child").addClass('lead');
+	$("pre").addClass('prettyprint');
+	prettyPrint();
+});
