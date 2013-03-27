20e8f926fa1b7210435d4ff94c5cf1012e97ce74
Matthieu Napoli <matthieu@mnapoli.fr>
2013-03-27 19:37:10 +0100

    Refactored PhpDoc parsing outside of AnnotationDefinitionReader

--- src/DI/Definition/AnnotationDefinitionReader.php
+++ src/DI/Definition/AnnotationDefinitionReader.php
@@ -10,15 +10,13 @@
 namespace DI\Definition;
 
 use DI\Annotations\Inject;
+use DI\Definition\Annotation\PhpDocParser;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\Common\Annotations\PhpParser;
 use Doctrine\Common\Annotations\Reader;
 use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionMethod;
-use ReflectionParameter;
-use ReflectionProperty;
 
 /**
  * Reads DI class definitions in annotations such as @ Inject and @ var annotations
@@ -34,14 +32,17 @@ class AnnotationDefinitionReader implements DefinitionReader
     private $annotationReader;
 
     /**
-     * @var PhpParser
+     * @var PhpDocParser
      */
-    private $phpParser;
+    private $phpDocParser;
 
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
-        $this->phpParser = new PhpParser();
+        $this->phpDocParser = new PhpDocParser();
     }
 
     /**
@@ -101,7 +102,7 @@ class AnnotationDefinitionReader implements DefinitionReader
                 $entryName = $annotation->getName();
                 if ($entryName == null) {
                     // Look for @var content
-                    $entryName = $this->getPropertyType($reflectionClass, $property);
+                    $entryName = $this->phpDocParser->getPropertyType($reflectionClass, $property);
                 }
                 $propertyInjection = new PropertyInjection($property->name, $entryName, $annotation->isLazy());
                 $classDefinition->addPropertyInjection($propertyInjection);
@@ -204,7 +205,7 @@ class AnnotationDefinitionReader implements DefinitionReader
 
             // Look for @param tag
             if ($entryName === null) {
-                $entryName = $this->getParameterType($reflectionClass, $method, $parameter);
+                $entryName = $this->phpDocParser->getParameterType($reflectionClass, $method, $parameter);
             }
 
             $methodInjection->addParameterInjection(new ParameterInjection($parameter->name, $entryName));
@@ -231,139 +232,6 @@ class AnnotationDefinitionReader implements DefinitionReader
     public function setAnnotationReader(Reader $annotationReader)
     {
         $this->annotationReader = $annotationReader;
-    }
-
-    /**
-     * Parse the docblock of the property to get the var annotation
-     * @param ReflectionClass    $class
-     * @param ReflectionProperty $property
-     * @throws AnnotationException
-     * @return string|null Type of the property (content of var annotation)
-     */
-    private function getPropertyType(ReflectionClass $class, ReflectionProperty $property)
-    {
-        // Get the content of the @var annotation
-        if (preg_match('/@var\s+([^\s]+)/', $property->getDocComment(), $matches)) {
-            list(, $type) = $matches;
-        } else {
-            return null;
-        }
-
-        // If the class name is not fully qualified (i.e. doesn't start with a \)
-        if ($type[0] !== '\\') {
-            $alias = (false === $pos = strpos($type, '\\')) ? $type : substr($type, 0, $pos);
-            $loweredAlias = strtolower($alias);
-
-            // Retrieve "use" statements
-            $uses = $this->phpParser->parseClass($property->getDeclaringClass());
-
-            $found = false;
-
-            if ($this->classExists($class->getNamespaceName() . '\\' . $type)) {
-                $type = $class->getNamespaceName() . '\\' . $type;
-                $found = true;
-            } elseif (isset($uses[$loweredAlias])) {
-                // Imported classes
-                if (false !== $pos) {
-                    $type = $uses[$loweredAlias] . substr($type, $pos);
-                } else {
-                    $type = $uses[$loweredAlias];
-                }
-                $found = true;
-            } elseif (isset($uses['__NAMESPACE__']) && $this->classExists($uses['__NAMESPACE__'] . '\\' . $type)) {
-                // Class namespace
-                $type = $uses['__NAMESPACE__'] . '\\' . $type;
-                $found = true;
-            } elseif ($this->classExists($type)) {
-                // No namespace
-                $found = true;
-            }
-
-            if (!$found) {
-                throw new AnnotationException("The @var annotation on {$class->name}::" . $property->getName()
-                    . " contains a non existent class. Did you maybe forget to add a 'use' statement for this annotation?");
-            }
-        }
-
-        if (!$this->classExists($type)) {
-            throw new AnnotationException("The @var annotation on {$class->name}::" . $property->getName()
-                . " contains a non existent class");
-        }
-
-        // Remove the leading \ (FQN shouldn't contain it)
-        $type = ltrim($type, '\\');
-
-        return $type;
-    }
-
-    /**
-     * Parse the docblock of the property to get the param annotation
-     * @param ReflectionClass  $class
-     * @param ReflectionMethod $method
-     * @throws AnnotationException
-     * @return string|null Type of the property (content of var annotation)
-     */
-    private function getParameterType(ReflectionClass $class, ReflectionMethod $method, ReflectionParameter $parameter)
-    {
-        // Use reflection
-        $parameterClass = $parameter->getClass();
-        if ($parameterClass !== null) {
-            return $parameterClass->name;
-        }
-
-        $parameterName = $parameter->name;
-        // Get the content of the @param annotation
-        if (preg_match('/@param\s+([^\s]+)\s+\$' . $parameterName . '/', $method->getDocComment(), $matches)) {
-            list(, $type) = $matches;
-        } else {
-            return null;
-        }
-
-        // If the class name is not fully qualified (i.e. doesn't start with a \)
-        if ($type[0] !== '\\') {
-            $alias = (false === $pos = strpos($type, '\\')) ? $type : substr($type, 0, $pos);
-            $loweredAlias = strtolower($alias);
-
-            // Retrieve "use" statements
-            $uses = $this->phpParser->parseClass($method->getDeclaringClass());
-
-            $found = false;
-
-            if ($this->classExists($class->getNamespaceName() . '\\' . $type)) {
-                $type = $class->getNamespaceName() . '\\' . $type;
-                $found = true;
-            } elseif (isset($uses[$loweredAlias])) {
-                // Imported classes
-                if (false !== $pos) {
-                    $type = $uses[$loweredAlias] . substr($type, $pos);
-                } else {
-                    $type = $uses[$loweredAlias];
-                }
-                $found = true;
-            } elseif (isset($uses['__NAMESPACE__']) && $this->classExists($uses['__NAMESPACE__'] . '\\' . $type)) {
-                // Class namespace
-                $type = $uses['__NAMESPACE__'] . '\\' . $type;
-                $found = true;
-            } elseif ($this->classExists($type)) {
-                // No namespace
-                $found = true;
-            }
-
-            if (!$found) {
-                throw new AnnotationException("The @param annotation for parameter $parameterName of {$class->name}::" . $method->name
-                    . " contains a non existent class. Did you maybe forget to add a 'use' statement for this annotation?");
-            }
-        }
-
-        if (!$this->classExists($type)) {
-            throw new AnnotationException("The @param annotation for parameter $parameterName of {$class->name}::" . $method->name
-                . " contains a non existent class");
-        }
-
-        // Remove the leading \ (FQN shouldn't contain it)
-        $type = ltrim($type, '\\');
-
-        return $type;
     }
 
     /**
