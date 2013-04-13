3b0c0a49d57e16806d097a5d099fea7d75f22970
Matthieu Napoli <matthieu@mnapoli.fr>
2013-04-13 19:01:45 +0200

    Changed website template

--- gh-pages-template/public/js/template.js
+++ gh-pages-template/public/js/template.js
@@ -1,22 +1,6 @@
-$(document).scroll(function(){
-    // If has not activated (has no attribute "data-top"
-    if (!$('.subnav').attr('data-top')) {
-        // If already fixed, then do nothing
-        if ($('.subnav').hasClass('subnav-fixed')) return;
-        // Remember top position
-        var offset = $('.subnav').offset();
-        $('.subnav').attr('data-top', offset.top);
-    }
-
-    if ($('.subnav').attr('data-top') + $('.subnav').outerHeight() <= $(this).scrollTop())
-        $('.subnav').addClass('subnav-fixed');
-    else
-        $('.subnav').removeClass('subnav-fixed');
-});
-
 $(function() {
-	$("section h1").wrap('<div class="page-header" />');
-	$("section p:first-child").addClass('lead');
+	$(".main h1").wrap('<div class="page-header" />');
+	$(".main > .container-fluid > p:first-child").addClass('lead').wrap('<div class="hero" />');
 	$("pre").addClass('prettyprint');
 	prettyPrint();
 });
