822ef83f7b6db2e9006894742d2fcdbfb014a069
Matthieu Napoli <matthieu@mnapoli.fr>
2013-04-23 18:36:37 +0200

    Improved tests

--- tests/UnitTests/DI/Definition/FileLoader/Fixtures/definitions.php
+++ tests/UnitTests/DI/Definition/FileLoader/Fixtures/definitions.php
@@ -7,37 +7,37 @@ return array(
     'value2' => 123,
     'value3' => true,
     'namespace\class1' => array(
-        'scope' => Scope::SINGLETON(),
+        'scope' => 'singleton',
         'lazy' => true,
         'constructor' => array(
             'name1' => 'argument1',
             'name2' => 'argument2',
-            'name3' => 'argument3'
+            'name3' => 'argument3',
         ),
         'methods' => array(
             'method1' => array(
                 'name4' => 'argument4',
-                'name5' => 'argument5'
+                'name5' => 'argument5',
             ),
             'method2' => array(
-                'name6' => 'argument6'
+                'name6' => 'argument6',
             ),
-            'method3' => array('argument7', 'argument8')
+            'method3' => array('argument7', 'argument8'),
         ),
         'properties' => array(
             'property1' => 'value1',
             'property2' => array(
                 'name' => 'interface1',
-                'lazy' => true
-            )
-        )
+                'lazy' => true,
+            ),
+        ),
     ),
     'interface1' => array(
         'class' => 'namespace\\class1',
-        'lazy' => false
+        'lazy' => false,
     ),
     'interface2' => array(
         'class' => 'class2',
-        'scope' => Scope::PROTOTYPE()
-    )
+        'scope' => 'prototype',
+    ),
 );
