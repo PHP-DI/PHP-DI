351826678861c4482f62274c3d58bd72f188fc75
Matthieu Napoli <matthieu@mnapoli.fr>
2013-12-21 18:40:20 +0100

    Improved tests for DefinitionManager

--- tests/UnitTests/DI/Definition/DefinitionManagerTest.php
+++ tests/UnitTests/DI/Definition/DefinitionManagerTest.php
@@ -10,6 +10,7 @@
 namespace UnitTests\DI\Definition;
 
 use DI\Definition\DefinitionManager;
+use DI\Definition\ValueDefinition;
 
 /**
  * Test class for DefinitionManager
@@ -22,7 +23,7 @@ class DefinitionManagerTest extends \PHPUnit_Framework_TestCase
      * @test
      * @covers \DI\Definition\DefinitionManager
      */
-    public function shouldUseCache()
+    public function shouldGetFromCache()
     {
         $definitionManager = new DefinitionManager();
 
@@ -33,6 +34,58 @@ class DefinitionManagerTest extends \PHPUnit_Framework_TestCase
 
         $definitionManager->setCache($cache);
 
+        $this->assertEquals($cache, $definitionManager->getCache());
+
         $this->assertEquals('foo', $definitionManager->getDefinition('foo'));
+    }
+    /**
+     * @test
+     * @covers \DI\Definition\DefinitionManager
+     */
+    public function shouldSaveToCache()
+    {
+        $definitionManager = new DefinitionManager();
+
+        $cache = $this->getMockForAbstractClass('Doctrine\Common\Cache\Cache');
+        $cache->expects($this->once())
+            ->method('fetch')
+            ->will($this->returnValue(false));
+        $cache->expects($this->once())
+            ->method('save');
+
+        $definitionManager->setCache($cache);
+
+        $this->assertNull($definitionManager->getDefinition('foo'));
+    }
+
+    /**
+     * Tests that the given definition source is chained to the ArraySource and used.
+     */
+    public function testDefinitionSource()
+    {
+        $definition = $this->getMockForAbstractClass('DI\Definition\Definition');
+        $definition->expects($this->once())
+            ->method('isCacheable')
+            ->will($this->returnValue(true));
+
+        $source = $this->getMockForAbstractClass('DI\Definition\Source\DefinitionSource');
+        $source->expects($this->once())
+            ->method('getDefinition')
+            ->with('foo')
+            ->will($this->returnValue($definition));
+
+        $definitionManager = new DefinitionManager($source);
+
+        $this->assertSame($definition, $definitionManager->getDefinition('foo'));
+    }
+
+    public function testAddDefinition()
+    {
+        $definitionManager = new DefinitionManager();
+        $valueDefinition = new ValueDefinition('foo', 'bar');
+
+        $definitionManager->addDefinition($valueDefinition);
+
+        $this->assertSame($valueDefinition, $definitionManager->getDefinition('foo'));
     }
 }
