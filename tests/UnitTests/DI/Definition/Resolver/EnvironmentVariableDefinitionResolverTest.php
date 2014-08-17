0b4ba8e7131a897b8bd0790c5bee205ba0007cbf
Matthieu Napoli <matthieu@mnapoli.fr>
2014-08-18 00:02:13 +0200

    Changed the URL to the project

--- tests/UnitTests/DI/Definition/Resolver/EnvironmentVariableDefinitionResolverTest.php
+++ tests/UnitTests/DI/Definition/Resolver/EnvironmentVariableDefinitionResolverTest.php
@@ -2,7 +2,7 @@
 /**
  * PHP-DI
  *
- * @link      http://mnapoli.github.io/PHP-DI/
+ * @link      http://php-di.org/
  * @copyright Matthieu Napoli (http://mnapoli.fr/)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
  */
