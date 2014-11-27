cbf775bcfc4220dfc62ab326eb879662f6ce0fca
Matthieu Napoli <matthieu@mnapoli.fr>
2014-11-27 17:40:03 +1300

    Fixed phpdoc that was lost on DI\* functions because of 0e267e2022

--- src/DI/functions.php
+++ src/DI/functions.php
@@ -14,59 +14,59 @@ use DI\Definition\Helper\FactoryDefinitionHelper;
 use DI\Definition\Helper\ClassDefinitionHelper;
 use DI\Definition\Helper\EnvironmentVariableDefinitionHelper;
 
-/**
- * Helper for defining an object.
- *
- * @param string|null $className Class name of the object.
- *                               If null, the name of the entry (in the container) will be used as class name.
- *
- * @return ClassDefinitionHelper
- */
 if (! function_exists('DI\object')) {
+    /**
+     * Helper for defining an object.
+     *
+     * @param string|null $className Class name of the object.
+     *                               If null, the name of the entry (in the container) will be used as class name.
+     *
+     * @return ClassDefinitionHelper
+     */
     function object($className = null)
     {
         return new ClassDefinitionHelper($className);
     }
 }
 
-/**
- * Helper for defining a container entry using a factory function/callable.
- *
- * @param callable $factory The factory is a callable that takes the container as parameter
- *                          and returns the value to register in the container.
- *
- * @return FactoryDefinitionHelper
- */
 if (! function_exists('DI\factory')) {
+    /**
+     * Helper for defining a container entry using a factory function/callable.
+     *
+     * @param callable $factory The factory is a callable that takes the container as parameter
+     *                          and returns the value to register in the container.
+     *
+     * @return FactoryDefinitionHelper
+     */
     function factory($factory)
     {
         return new FactoryDefinitionHelper($factory);
     }
 }
 
-/**
- * Helper for referencing another container entry in an object definition.
- *
- * @param string $entryName
- *
- * @return EntryReference
- */
 if (! function_exists('DI\link')) {
+    /**
+     * Helper for referencing another container entry in an object definition.
+     *
+     * @param string $entryName
+     *
+     * @return EntryReference
+     */
     function link($entryName)
     {
         return new EntryReference($entryName);
     }
 }
 
-/**
- * Helper for referencing environment variables.
- *
- * @param string $variableName The name of the environment variable.
- * @param mixed $defaultValue The default value to be used if the environment variable is not defined.
- *
- * @return EnvironmentVariableDefinitionHelper
- */
 if (! function_exists('DI\env')) {
+    /**
+     * Helper for referencing environment variables.
+     *
+     * @param string $variableName The name of the environment variable.
+     * @param mixed $defaultValue The default value to be used if the environment variable is not defined.
+     *
+     * @return EnvironmentVariableDefinitionHelper
+     */
     function env($variableName, $defaultValue = null)
     {
         // Only mark as optional if the default value was *explicitly* provided.
