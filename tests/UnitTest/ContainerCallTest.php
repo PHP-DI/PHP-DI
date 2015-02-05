b708590e966fe28f2161c45ae721242452f2ef8a
Matthieu Napoli <matthieu@mnapoli.fr>
2015-02-05 16:46:55 +1300

    More tests for Container::call()

--- tests/UnitTest/ContainerCallTest.php
+++ tests/UnitTest/ContainerCallTest.php
@@ -18,7 +18,10 @@ use DI\ContainerBuilder;
  */
 class ContainerCallTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCallNoParameters()
+    /**
+     * @test
+     */
+    public function call_should_invoke_closures()
     {
         $container = ContainerBuilder::buildDevContainer();
 
@@ -27,5 +30,74 @@ class ContainerCallTest extends \PHPUnit_Framework_TestCase
         });
 
         $this->assertEquals('foo', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function call_should_invoke_array_callables_on_objects()
+    {
+        $container = ContainerBuilder::buildDevContainer();
+
+        $result = $container->call(array($this, 'method'));
+
+        $this->assertEquals('foo', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function call_should_invoke_array_callables_on_classes()
+    {
+        $container = ContainerBuilder::buildDevContainer();
+
+        $result = $container->call(array(get_class(), 'staticMethod'));
+
+        $this->assertEquals('bar', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function call_should_pass_parameters()
+    {
+        $container = ContainerBuilder::buildDevContainer();
+
+        $result = $container->call(function ($param1, $param2) {
+            return $param1 . $param2;
+        }, array(
+            'param1' => 'foo',
+            'param2' => 'bar',
+        ));
+
+        $this->assertEquals('foobar', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function parameters_should_be_indexed_by_name()
+    {
+        $container = ContainerBuilder::buildDevContainer();
+
+        $result = $container->call(function ($param1, $param2) {
+            return $param1 . $param2;
+        }, array(
+            // Reverse order: should still work
+            'param2' => 'bar',
+            'param1' => 'foo',
+        ));
+
+        $this->assertEquals('foobar', $result);
+    }
+
+    public function method()
+    {
+        return 'foo';
+    }
+
+    public static function staticMethod()
+    {
+        return 'bar';
     }
 }
