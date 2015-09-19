df6b5ffa90c951b443b36025ae09c4e01d4a7896
Matthieu Napoli <matthieu@mnapoli.fr>
2015-09-19 15:03:35 +0200

    Remove file docblocks

--- src/DI/Definition/CacheableDefinition.php
+++ src/DI/Definition/CacheableDefinition.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * PHP-DI
- *
- * @link      http://php-di.org/
- * @copyright Matthieu Napoli (http://mnapoli.fr/)
- * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
- */
 
 namespace DI\Definition;
 
--- src/DI/Definition/Resolver/DecoratorResolver.php
+++ src/DI/Definition/Resolver/DecoratorResolver.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * PHP-DI
- *
- * @link      http://mnapoli.github.com/PHP-DI/
- * @copyright Matthieu Napoli (http://mnapoli.fr/)
- * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
- */
 
 namespace DI\Definition\Resolver;
 
