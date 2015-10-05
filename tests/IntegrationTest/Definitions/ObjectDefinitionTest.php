3342d97ce05d6b387c33b0852e5b5c4b19dd1cc2
Matthieu Napoli <matthieu@mnapoli.fr>
2015-10-05 19:18:46 +0200

    Merge integration tests

--- tests/IntegrationTest/Definitions/ObjectDefinitionTest.php
+++ tests/IntegrationTest/Definitions/ObjectDefinitionTest.php
@@ -17,15 +17,14 @@ class ObjectDefinitionTest extends \PHPUnit_Framework_TestCase
     {
         $builder = new ContainerBuilder();
         $builder->useAutowiring(false);
-        $builder->useAnnotations(false);
         $builder->addDefinitions([
             // with the same name
-            'stdClass' => \DI\object('stdClass'),
+            'stdClass'    => \DI\object('stdClass'),
             // with name inferred
             'ArrayObject' => \DI\object()
                 ->constructor([]),
             // with a different name
-            'object' => \DI\object('ArrayObject')
+            'object'      => \DI\object('ArrayObject')
                 ->constructor([]),
         ]);
         $container = $builder->build();
@@ -33,5 +32,41 @@ class ObjectDefinitionTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('stdClass', $container->get('stdClass'));
         $this->assertInstanceOf('ArrayObject', $container->get('object'));
         $this->assertInstanceOf('ArrayObject', $container->get('ArrayObject'));
+    }
+
+    public function test_multiple_method_call()
+    {
+        $builder = new ContainerBuilder();
+        $builder->useAutowiring(false);
+        $builder->addDefinitions([
+            __NAMESPACE__ . '\ObjectDefinition\Class1' => \DI\object()
+                ->method('increment')
+                ->method('increment'),
+        ]);
+        $container = $builder->build();
+
+        $class = $container->get(__NAMESPACE__ . '\ObjectDefinition\Class1');
+        $this->assertEquals(2, $class->count);
+    }
+
+    public function test_override_parameter_with_multiple_method_call()
+    {
+        $builder = new ContainerBuilder();
+        $builder->useAutowiring(false);
+        $builder->addDefinitions([
+            __NAMESPACE__ . '\ObjectDefinition\Class1' => \DI\object()
+                ->method('add', 'foo')
+                ->method('add', 'foo'),
+        ]);
+        $builder->addDefinitions([
+            // Override a method parameter
+            __NAMESPACE__ . '\ObjectDefinition\Class1' => \DI\object()
+                ->methodParameter('add', 0, 'bar'),
+        ]);
+        $container = $builder->build();
+
+        // Should override only the first method call
+        $class = $container->get(__NAMESPACE__ . '\ObjectDefinition\Class1');
+        $this->assertEquals(['bar', 'foo'], $class->items);
     }
 }
