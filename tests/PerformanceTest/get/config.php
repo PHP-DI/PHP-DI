4d0ca45d3e40ce4fb4ef895154d5be3639c57b1a
Matthieu Napoli <matthieu@mnapoli.fr>
2015-12-27 11:02:48 -0500

    Applied fixes from StyleCI

--- tests/PerformanceTest/get/config.php
+++ tests/PerformanceTest/get/config.php
@@ -3,10 +3,10 @@
 use Interop\Container\ContainerInterface;
 
 return [
-    'object' => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'value' => 'foo',
-    'string' => \DI\string('Hello this is {value}'),
-    'alias' => \DI\get('string'),
+    'object'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
+    'value'   => 'foo',
+    'string'  => \DI\string('Hello this is {value}'),
+    'alias'   => \DI\get('string'),
     'factory' => \DI\factory(function (ContainerInterface $c) {
         return $c->get('object');
     }),
