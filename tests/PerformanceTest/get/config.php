f76dfdb1713bdadf765c60ba3b7950d8868f7fab
Matthieu Napoli <matthieu@mnapoli.fr>
2015-12-27 13:20:52 +0100

    Add more performance tests

--- tests/PerformanceTest/get/config.php
+++ tests/PerformanceTest/get/config.php
@@ -1,14 +1,18 @@
 <?php
 
+use Interop\Container\ContainerInterface;
+
 return [
-    'object1'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object2'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object3'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object4'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object5'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object6'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object7'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object8'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object9'  => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
-    'object10' => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
+    'object' => \DI\object('DI\Test\PerformanceTest\Get\GetFixture'),
+    'value' => 'foo',
+    'string' => \DI\string('Hello this is {value}'),
+    'alias' => \DI\get('string'),
+    'factory' => \DI\factory(function (ContainerInterface $c) {
+        return $c->get('object');
+    }),
+    'array' => [
+        'foo',
+        'bar',
+        \DI\get('value'),
+    ],
 ];
