ba13e2a9ccc4cbb35bc62959cc41bf55abfeb2b5
Menno Holtkamp <menno.holtkamp@gmail.com>
2016-05-21 17:32:21 +0200

    Explicitly set FileWriterGeneratorStrategy
    It defaults to the EvaluatingGeneratorStrategy

--- src/DI/Proxy/ProxyFactory.php
+++ src/DI/Proxy/ProxyFactory.php
@@ -4,7 +4,8 @@ namespace DI\Proxy;
 
 use ProxyManager\Configuration;
 use ProxyManager\Factory\LazyLoadingValueHolderFactory;
-use ProxyManager\GeneratorStrategy\EvaluatingGeneratorStrategy;
+use ProxyManager\FileLocator\FileLocator;
+use ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy;
 
 /**
  * Creates proxy classes.
@@ -71,9 +72,8 @@ class ProxyFactory
 
         if ($this->writeProxiesToFile) {
             $config->setProxiesTargetDir($this->proxyDirectory);
+            $config->setGeneratorStrategy(new FileWriterGeneratorStrategy(new FileLocator($this->proxyDirectory)));
             spl_autoload_register($config->getProxyAutoloader());
-        } else {
-            $config->setGeneratorStrategy(new EvaluatingGeneratorStrategy());
         }
 
         $this->proxyManager = new LazyLoadingValueHolderFactory($config);
