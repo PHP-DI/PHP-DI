f24d7498c3c6d702da2765be10c389cf93e447ca
Matthieu Napoli <matthieu@mnapoli.fr>
2017-01-04 21:40:06 +0100

    Add more tests on ObjectDefinition

--- tests/UnitTest/Definition/ObjectDefinitionTest.php
+++ tests/UnitTest/Definition/ObjectDefinitionTest.php
@@ -4,6 +4,8 @@ namespace DI\Test\UnitTest\Definition;
 
 use DI\Definition\CacheableDefinition;
 use DI\Definition\ObjectDefinition;
+use DI\Definition\ObjectDefinition\MethodInjection;
+use DI\Definition\ObjectDefinition\PropertyInjection;
 use DI\Scope;
 use DI\Test\UnitTest\Definition\Fixture\NonInstantiableClass;
 
@@ -56,6 +58,31 @@ class ObjectDefinitionTest extends \PHPUnit_Framework_TestCase
         self::assertFalse($definition->isInstantiable());
         $definition = new ObjectDefinition(\stdClass::class);
         self::assertTrue($definition->classExists());
+    }
+
+    public function test_property_injections()
+    {
+        $definition = new ObjectDefinition('foo');
+        $definition->addPropertyInjection(new PropertyInjection('property1', 'Property1'));
+        $definition->addPropertyInjection(new PropertyInjection('property2', 'Property2'));
+        $this->assertEquals([
+            'property1' => new PropertyInjection('property1', 'Property1'),
+            'property2' => new PropertyInjection('property2', 'Property2'),
+        ], $definition->getPropertyInjections());
+    }
+
+    public function test_method_injections()
+    {
+        $definition = new ObjectDefinition('foo');
+        $definition->setConstructorInjection(MethodInjection::constructor());
+        $definition->addMethodInjection(new MethodInjection('method1', ['foo']));
+        $definition->addMethodInjection(new MethodInjection('method2'));
+
+        $this->assertNotNull($definition->getConstructorInjection());
+        $this->assertEquals([
+            new MethodInjection('method1', ['foo']),
+            new MethodInjection('method2'),
+        ], $definition->getMethodInjections());
     }
 
     /**
