15570262d4d4f36a313e59c1716405c2ec43819d
Matthieu Napoli <matthieu@mnapoli.fr>
2017-05-30 22:17:49 +0200

    Cover exception messages for the compiled container

--- tests/IntegrationTest/ErrorMessages/ErrorMessagesTest.php
+++ tests/IntegrationTest/ErrorMessages/ErrorMessagesTest.php
@@ -4,13 +4,17 @@ namespace DI\Test\IntegrationTest\ErrorMessages;
 
 use DI\ContainerBuilder;
 use DI\Definition\Exception\InvalidDefinition;
+use DI\Test\IntegrationTest\BaseContainerTest;
 
 /**
- * @coversNothing
+ * Test error messages.
  */
-class ErrorMessagesTest extends \PHPUnit_Framework_TestCase
+class ErrorMessagesTest extends BaseContainerTest
 {
-    public function test_non_instantiable_class()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_non_instantiable_class(ContainerBuilder $builder)
     {
         $message = <<<'MESSAGE'
 Entry "DI\Test\IntegrationTest\ErrorMessages\InterfaceFixture" cannot be resolved: the class is not instantiable
@@ -21,13 +25,16 @@ Object (
     lazy = false
 )
 MESSAGE;
-        $this->setExpectedException(InvalidDefinition::class, $message);
+        $this->expectException(InvalidDefinition::class);
+        $this->expectExceptionMessage($message);
 
-        $container = ContainerBuilder::buildDevContainer();
-        $container->get('DI\Test\IntegrationTest\ErrorMessages\InterfaceFixture');
+        $builder->build()->get('DI\Test\IntegrationTest\ErrorMessages\InterfaceFixture');
     }
 
-    public function test_non_existent_class()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_non_existent_class(ContainerBuilder $builder)
     {
         $message = <<<'MESSAGE'
 Entry "Acme\Foo\Bar\Bar" cannot be resolved: the class doesn't exist
@@ -40,12 +47,15 @@ Object (
 MESSAGE;
         $this->setExpectedException(InvalidDefinition::class, $message);
 
-        $container = ContainerBuilder::buildDevContainer();
+        $container = $builder->build();
         $container->set('Acme\Foo\Bar\Bar', \DI\create());
         $container->get('Acme\Foo\Bar\Bar');
     }
 
-    public function test_undefined_constructor_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_undefined_constructor_parameter(ContainerBuilder $builder)
     {
         $message = <<<'MESSAGE'
 Entry "DI\Test\IntegrationTest\ErrorMessages\Buggy1" cannot be resolved: Parameter $bar of __construct() has no value defined or guessable
@@ -63,46 +73,49 @@ Object (
 MESSAGE;
         $this->setExpectedException(InvalidDefinition::class, $message);
 
-        $container = ContainerBuilder::buildDevContainer();
+        $container = $builder->build();
         $container->set(Buggy1::class, \DI\autowire()->constructorParameter('foo', 'some value'));
 
         $container->get(Buggy1::class);
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\DependencyException
      * @expectedExceptionMessage Error while injecting dependencies into DI\Test\IntegrationTest\ErrorMessages\Buggy2: No entry or class found for 'nonExistentEntry'
      */
-    public function test_constructor_injection_of_non_existent_container_entry()
+    public function test_constructor_injection_of_non_existent_container_entry(ContainerBuilder $builder)
     {
-        $builder = new ContainerBuilder();
         $builder->useAnnotations(true);
         $builder->build()->get(Buggy2::class);
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\DependencyException
      * @expectedExceptionMessage Error while injecting in DI\Test\IntegrationTest\ErrorMessages\Buggy3::dependency. No entry or class found for 'namedDependency'
      */
-    public function test_property_injection_of_non_existent_container_entry()
+    public function test_property_injection_of_non_existent_container_entry(ContainerBuilder $builder)
     {
-        $builder = new ContainerBuilder();
         $builder->useAnnotations(true);
         $builder->build()->get(Buggy3::class);
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\DependencyException
      * @expectedExceptionMessage Error while injecting dependencies into DI\Test\IntegrationTest\ErrorMessages\Buggy4: No entry or class found for 'nonExistentBean'
      */
-    public function test_setter_injection_of_non_existent_container_entry()
+    public function test_setter_injection_of_non_existent_container_entry(ContainerBuilder $builder)
     {
-        $builder = new ContainerBuilder();
         $builder->useAnnotations(true);
         $builder->build()->get(Buggy4::class);
     }
 
-    public function test_setter_injection_not_type_hinted()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_setter_injection_not_type_hinted(ContainerBuilder $builder)
     {
         $message = <<<'MESSAGE'
 Entry "DI\Test\IntegrationTest\ErrorMessages\Buggy5" cannot be resolved: Parameter $dependency of setDependency() has no value defined or guessable
@@ -118,18 +131,18 @@ Object (
 MESSAGE;
         $this->setExpectedException(InvalidDefinition::class, $message);
 
-        $builder = new ContainerBuilder();
         $builder->useAnnotations(true);
         $builder->build()->get(Buggy5::class);
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\Definition\Exception\InvalidDefinition
      * @expectedExceptionMessage Entry "foo" cannot be resolved: factory 'bar' is neither a callable nor a valid container entry
      */
-    public function test_factory_not_callable()
+    public function test_factory_not_callable(ContainerBuilder $builder)
     {
-        $container = ContainerBuilder::buildDevContainer();
+        $container = $builder->build();
         $container->set('foo', \DI\factory('bar'));
         $container->get('foo');
     }
