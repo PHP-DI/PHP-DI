afa026ffbad16052d8cec2727fe27dfb825827b1
Matthieu Napoli <matthieu@mnapoli.fr>
2017-06-03 12:13:33 +0200

    Cover factory definitions for the compiled container

--- tests/IntegrationTest/Definitions/FactoryDefinitionTest.php
+++ tests/IntegrationTest/Definitions/FactoryDefinitionTest.php
@@ -4,92 +4,113 @@ namespace DI\Test\IntegrationTest\Definitions;
 
 use DI\ContainerBuilder;
 use DI\Factory\RequestedEntry;
+use DI\Test\IntegrationTest\BaseContainerTest;
 use DI\Test\UnitTest\Definition\Resolver\Fixture\NoConstructor;
 use Psr\Container\ContainerInterface;
 
 /**
  * Test factory definitions.
  */
-class FactoryDefinitionTest extends \PHPUnit_Framework_TestCase
+class FactoryDefinitionTest extends BaseContainerTest
 {
-    public function provideCallables()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_closure_shortcut(ContainerBuilder $builder)
     {
-        return [
-            'closure'               => [function () {
-                return 'bar';
-            }],
-            'function'              => [__NAMESPACE__ . '\FactoryDefinition_test'],
-            'invokableObject'       => [new FactoryDefinitionInvokableTestClass],
-            'invokableClass'        => [FactoryDefinitionInvokableTestClass::class],
-            '[Class, staticMethod]' => [[FactoryDefinitionTestClass::class, 'staticFoo']],
-            'Class::staticMethod'   => [FactoryDefinitionTestClass::class . '::staticFoo'],
-            '[object, method]'      => [[new FactoryDefinitionTestClass, 'foo']],
-            '[class, method]'       => [[FactoryDefinitionTestClass::class, 'foo']],
-            'class::method'         => [FactoryDefinitionTestClass::class . '::foo'],
-        ];
-    }
-
-    public function provideNamedContainerEntryCallables()
-    {
-        return [
-            '[arbitraryClassEntry, method]' => [['bar_baz', 'foo']],
-            'arbitraryClassEntry::method'   => ['bar_baz::foo'],
-        ];
-    }
-
-    public function test_closure_shortcut()
-    {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => function () {
                 return 'bar';
             },
         ]);
 
-        $this->assertEquals('bar', $container->get('factory'));
+        $this->assertEquals('bar', $builder->build()->get('factory'));
+    }
+
+    public function provideCallables()
+    {
+        $callables = [
+            'closure' => function () {
+                return 'bar';
+            },
+            'function' => __NAMESPACE__ . '\FactoryDefinition_test',
+            'invokableObject' => new FactoryDefinitionInvokableTestClass,
+            'invokableClass' => FactoryDefinitionInvokableTestClass::class,
+            '[Class, staticMethod]' => [FactoryDefinitionTestClass::class, 'staticFoo'],
+            'Class::staticMethod' => FactoryDefinitionTestClass::class . '::staticFoo',
+            '[object, method]' => [new FactoryDefinitionTestClass, 'foo'],
+            '[class, method]' => [FactoryDefinitionTestClass::class, 'foo'],
+            'class::method' => FactoryDefinitionTestClass::class . '::foo',
+        ];
+
+        $testCases = [];
+        foreach ($callables as $callableName => $callable) {
+            foreach ($this->provideContainer() as $containerName => $container) {
+                $testCases[$containerName . ' - ' . $callableName] = [$callable, clone $container[0]];
+            }
+        }
+
+        return $testCases;
     }
 
     /**
      * @dataProvider provideCallables
      */
-    public function test_factory($callable)
+    public function test_factory($callable, ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory($callable),
         ]);
 
-        $this->assertSame('bar', $container->get('factory'));
+        $this->assertSame('bar', $builder->build()->get('factory'));
     }
 
     /**
-     * @dataProvider provideNamedContainerEntryCallables
+     * @dataProvider provideContainer
      */
-    public function test_named_container_entry_as_factory($callable)
+    public function test_named_container_entry_as_factory(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'bar_baz' => \DI\create(FactoryDefinitionTestClass::class),
-            'factory' => \DI\factory($callable),
+            'factory' => \DI\factory(['bar_baz', 'foo']),
         ]);
 
-        $this->assertSame('bar', $container->get('factory'));
+        $this->assertSame('bar', $builder->build()->get('factory'));
     }
 
-    public function test_named_invokable_container_entry_as_factory()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_named_container_entry_as_factory_with_string_callable(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
+            'bar_baz' => \DI\create(FactoryDefinitionTestClass::class),
+            'factory' => \DI\factory('bar_baz::foo'),
+        ]);
+
+        $this->assertSame('bar', $builder->build()->get('factory'));
+    }
+
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_named_invokable_container_entry_as_factory(ContainerBuilder $builder)
+    {
+        $builder->addDefinitions([
             'bar_baz' => \DI\create(FactoryDefinitionInvokableTestClass::class),
             'factory' => \DI\factory('bar_baz'),
         ]);
 
-        $this->assertSame('bar', $container->get('factory'));
+        $this->assertSame('bar', $builder->build()->get('factory'));
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\Definition\Exception\InvalidDefinition
      * @expectedExceptionMessage Invokable classes cannot be automatically resolved if autowiring is disabled on the container, you need to enable autowiring or define the entry manually.
      */
-    public function test_error_message_on_invokable_class_without_autowiring()
+    public function test_error_message_on_invokable_class_without_autowiring(ContainerBuilder $builder)
     {
-        $builder = new ContainerBuilder();
         $builder->addDefinitions([
             'factory' => \DI\factory(FactoryDefinitionInvokableTestClass::class),
         ]);
@@ -98,143 +119,176 @@ class FactoryDefinitionTest extends \PHPUnit_Framework_TestCase
         $container->get('factory');
     }
 
-    public function test_container_gets_injected_as_first_argument_without_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_container_gets_injected_as_first_argument_without_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => function ($c) {
                 return $c;
             },
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf(ContainerInterface::class, $factory);
     }
 
-    public function test_requested_entry_gets_injected_as_second_argument_without_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_requested_entry_gets_injected_as_second_argument_without_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'foobar' => function ($c, $entry) {
                 return $entry->getName();
             },
         ]);
 
-        $factory = $container->get('foobar');
+        $factory = $builder->build()->get('foobar');
 
         $this->assertSame('foobar', $factory);
     }
 
-    public function test_requested_entry_gets_injected_with_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_requested_entry_gets_injected_with_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'foobar' => function (RequestedEntry $entry) {
                 return $entry->getName();
             },
         ]);
 
-        $factory = $container->get('foobar');
+        $factory = $builder->build()->get('foobar');
 
         $this->assertSame('foobar', $factory);
     }
 
-    public function test_arbitrary_object_gets_injected_via_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_arbitrary_object_gets_injected_via_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => function (\stdClass $stdClass) {
                 return $stdClass;
             },
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf('stdClass', $factory);
     }
 
-    public function test_container_and_requested_entry_get_injected_in_arbitrary_position_via_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_container_and_requested_entry_get_injected_in_arbitrary_position_via_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => function (\stdClass $stdClass, RequestedEntry $e, ContainerInterface $c) {
                 return [$stdClass, $e, $c];
             },
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf('stdClass', $factory[0]);
         $this->assertInstanceOf(RequestedEntry::class, $factory[1]);
         $this->assertInstanceOf(ContainerInterface::class, $factory[2]);
     }
 
-    public function test_container_get_injected_in_arbitrary_position_via_typehint()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_container_get_injected_in_arbitrary_position_via_typehint(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => function (\stdClass $stdClass, ContainerInterface $c) {
                 return [$stdClass, $c];
             },
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf('stdClass', $factory[0]);
         $this->assertInstanceOf(ContainerInterface::class, $factory[1]);
     }
 
-    public function test_value_gets_injected_via_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_value_gets_injected_via_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function ($value) {
                 return $value;
             })->parameter('value', 'Foo'),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertEquals('Foo', $factory);
     }
 
-    public function test_named_entry_gets_injected_via_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_named_entry_gets_injected_via_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'basicClass' => \DI\create(\stdClass::class),
             'factory' => \DI\factory(function ($entry) {
                 return $entry;
             })->parameter('entry', \DI\get('basicClass')),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf(\stdClass::class, $factory);
     }
 
-    public function test_sub_entry_gets_injected_via_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_sub_entry_gets_injected_via_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function ($entry) {
                 return $entry;
             })->parameter('entry', \DI\create(\stdClass::class)),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf(\stdClass::class, $factory);
     }
 
-    public function test_class_gets_injected_via_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_class_gets_injected_via_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function ($entry) {
                 return $entry;
             })->parameter('entry', \DI\get(\stdClass::class)),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf(\stdClass::class, $factory);
     }
 
-    public function test_multiple_injections_via_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_multiple_injections_via_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'secret' => 'Bar',
             'factory' => \DI\factory(function ($a, $b, $c) {
                 return [$a, $b, $c];
@@ -243,23 +297,26 @@ class FactoryDefinitionTest extends \PHPUnit_Framework_TestCase
               ->parameter('c', 'Foo'),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertEquals('Bar', $factory[0]);
         $this->assertInstanceOf(FactoryDefinitionTestClass::class, $factory[1]);
         $this->assertEquals('Foo', $factory[2]);
     }
 
-    public function test_container_and_requested_entry_and_typehints_get_injected_with_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_container_and_requested_entry_and_typehints_get_injected_with_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'secret' => 'Bar',
             'factory' => \DI\factory(function ($container, $requestedEntry, \stdClass $object, $value) {
                 return [$container, $requestedEntry, $object, $value];
             })->parameter('value', \DI\get('secret')),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf(ContainerInterface::class, $factory[0]);
         $this->assertInstanceOf(RequestedEntry::class, $factory[1]);
@@ -267,16 +324,19 @@ class FactoryDefinitionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Bar', $factory[3]);
     }
 
-    public function test_container_and_requested_entry_and_typehints_get_injected_in_arbitrary_positions_with_parameter()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_container_and_requested_entry_and_typehints_get_injected_in_arbitrary_positions_with_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'secret' => 'Bar',
             'factory' => \DI\factory(function (\stdClass $object, RequestedEntry $requestedEntry, $value, ContainerInterface $container) {
                 return [$object, $requestedEntry, $value, $container];
             })->parameter('value', \DI\get('secret')),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertInstanceOf('stdClass', $factory[0]);
         $this->assertInstanceOf(RequestedEntry::class, $factory[1]);
@@ -284,66 +344,66 @@ class FactoryDefinitionTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(ContainerInterface::class, $factory[3]);
     }
 
-    public function test_parameters_take_priority_over_container()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_parameters_take_priority_over_container(ContainerBuilder $builder)
     {
         $ncInst = new NoConstructor();
 
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function (NoConstructor $nc) {
                 return $nc;
             })->parameter('nc', $ncInst),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertSame($ncInst, $factory);
     }
 
-    public function test_parameters_take_priority_over_default_value()
+    /**
+     * @dataProvider provideContainer
+     */
+    public function test_parameters_take_priority_over_default_value(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function ($foo = 'Foo') {
                 return $foo;
             })->parameter('foo', 'Bar'),
         ]);
 
-        $factory = $container->get('factory');
+        $factory = $builder->build()->get('factory');
 
         $this->assertEquals('Bar', $factory);
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\NotFoundException
      * @expectedExceptionMessage No entry or class found for 'missing'
      */
-    public function test_resolve_failure_on_parameter()
+    public function test_resolve_failure_on_parameter(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'factory' => \DI\factory(function ($foo) {
                 return $foo;
             })->parameter('foo', \DI\get('missing')),
         ]);
-        $container->get('factory');
+        $builder->build()->get('factory');
     }
 
     /**
+     * @dataProvider provideContainer
      * @expectedException \DI\Definition\Exception\InvalidDefinition
      * @expectedExceptionMessage Entry "foo" cannot be resolved: factory 'Hello World' is neither a callable nor a valid container entry
      */
-    public function test_not_callable_factory_definition()
+    public function test_not_callable_factory_definition(ContainerBuilder $builder)
     {
-        $container = $this->createContainer([
+        $builder->addDefinitions([
             'foo' => \DI\factory('Hello World'),
         ]);
-        $container->get('foo');
-    }
-
-    private function createContainer(array $definitions)
-    {
-        $builder = new ContainerBuilder();
-        $builder->addDefinitions($definitions);
-
-        return $builder->build();
+        $builder->build()->get('foo');
     }
 }
 
