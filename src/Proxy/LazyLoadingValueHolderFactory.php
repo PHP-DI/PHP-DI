b15f8e78bf1851dd4f501a41701a4b9a9c71c864
Bela Bagameri <bbagameri@wayfair.com>
2019-02-18 10:59:49 +0100

    Formatting issues fixed

--- src/Proxy/LazyLoadingValueHolderFactory.php
+++ src/Proxy/LazyLoadingValueHolderFactory.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace DI\Proxy;
 
 /**
- * Class LazyLoadingValueHolderFactory
- *
  * To expose the least amount of the ProxyManager's internals we are extending the LazyLoadingValueHolderFactory
- * The protected `generateProxy` method will generate AND write the proxy classes to disk
- *
+ * The protected `generateProxy` method will generate AND write the proxy classes to disk.
  */
 class LazyLoadingValueHolderFactory extends \ProxyManager\Factory\LazyLoadingValueHolderFactory
 {
