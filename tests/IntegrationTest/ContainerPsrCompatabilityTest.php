4fd0bb4d0228ff11fed3232bf66b01a2e5c27a59
Martins Sipenko <martins.sipenko@gmail.com>
2021-04-20 13:15:15 +0300

    Make InvalidDefinition exception implement ContainerExceptionInterface

---
+++ tests/IntegrationTest/ContainerPsrCompatabilityTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DI\Test\IntegrationTest;
+
+use DI\ContainerBuilder;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
+use DI\Test\IntegrationTest\Fixtures\Class1;
+
+class ContainerPsrCompatabilityTest extends BaseContainerTest
+{
+    /**
+     * @dataProvider provideContainer
+     */
+    public function testNotFound(ContainerBuilder $builder)
+    {
+        $this->expectException(NotFoundExceptionInterface::class);
+        $builder->build()->get('key');
+    }
+
+    /**
+     * @dataProvider provideContainer
+     */
+    public function testUnresolvable(ContainerBuilder $builder)
+    {
+        $this->expectException(ContainerExceptionInterface::class);
+        $builder->build()->get(Class1::class);
+    }
+}
