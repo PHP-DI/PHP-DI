0001956beb613a5c43c7948aab09be827049a4c4
wilrik <wmook@050media.nl>
2023-08-31 15:30:02 +0200

    Added happy path test for wildcard definition in global namespace

--- tests/UnitTest/Definition/Source/DefinitionArrayTest.php
+++ tests/UnitTest/Definition/Source/DefinitionArrayTest.php
@@ -180,6 +180,7 @@ class DefinitionArrayTest extends TestCase
             'Namespaced\*Interface'  => \DI\create('Namespaced\*'),
             'Namespaced2\*Interface' => \DI\create('Namespaced2\Foo'),
             'Multiple\*\*\Matches'   => \DI\create('Multiple\*\*\Implementation'),
+            '*Interface'   => \DI\create('GlobalImplementation'),
         ]);
 
         $definition = $source->getDefinition('foo1');
@@ -201,6 +202,11 @@ class DefinitionArrayTest extends TestCase
         $this->assertInstanceOf(ObjectDefinition::class, $definition);
         $this->assertEquals('Multiple\Foo\Bar\Matches', $definition->getName());
         $this->assertEquals('Multiple\Foo\Bar\Implementation', $definition->getClassName());
+
+        $definition = $source->getDefinition('GlobalInterface');
+        $this->assertInstanceOf(ObjectDefinition::class, $definition);
+        $this->assertEquals('GlobalInterface', $definition->getName());
+        $this->assertEquals('GlobalImplementation', $definition->getClassName());
     }
 
     /**
