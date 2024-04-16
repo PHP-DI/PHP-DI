74a335de9794cc3955b915f94f34789942a7abb5
Serge Kvashnin <75180587+serge-kvashnin@users.noreply.github.com>
2024-04-17 00:10:43 +0200

    Resolve variadic parameters (https://github.com/PHP-DI/PHP-DI/issues/881).

--- src/Definition/Resolver/ParameterResolver.php
+++ src/Definition/Resolver/ParameterResolver.php
@@ -48,6 +48,26 @@ class ParameterResolver
                 // Look in the $parameters array
                 $value = &$parameters[$parameter->getName()];
             } elseif (array_key_exists($index, $definitionParameters)) {
+                if ($parameter->isVariadic()) {
+                    $args = array_merge(
+                        $args,
+                        // Resolve definitions, if any
+                        array_map(
+                            function ($arg) {
+                                if ($arg instanceof Definition) {
+                                    return $this->definitionResolver->resolve($arg);
+                                }
+
+                                return $arg;
+                            },
+                            // Parameters that are left
+                            array_slice($definitionParameters, $index)
+                        )
+                    );
+
+                    break;
+                }
+
                 // Look in the definition
                 $value = &$definitionParameters[$index];
             } else {
--- tests/UnitTest/Definition/Resolver/ObjectCreatorTest.php
+++ tests/UnitTest/Definition/Resolver/ObjectCreatorTest.php
@@ -54,6 +54,18 @@ class ObjectCreatorTest extends TestCase
         $this->assertEquals('value3', $object->methodParam1);
     }
 
+    public function testResolveWithVariadicParameter()
+    {
+        $definition = new ObjectDefinition(FixtureClass::class);
+        $definition->setConstructorInjection(MethodInjection::constructor(['value1']));
+        $definition->addMethodInjection(new MethodInjection('methodWithVariadicParameter', ['value1', 'value2', 'value3']));
+
+        $object = $this->resolver->resolve($definition);
+
+        $this->assertInstanceOf(FixtureClass::class, $object);
+        $this->assertEquals(['value1', 'value2', 'value3'], $object->methodParam3);
+    }
+
     public function testResolveNoConstructorClass()
     {
         $definition = new ObjectDefinition(NoConstructor::class);
